e55de5cec31cd98a38ef0f77ceae39ab69e6b5f7
Prerna6060 <112013748+Prerna6060@users.noreply.github.com>
2023-01-07 22:47:54 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import "./App.css";
 import Sidebar from "./Sidebar";
 import Chat from "./Chat";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div className="app">
       <div className="app_body">
         <Router>
-          <Routes>
+          <Switch>
             <Route path="/app">
               <Sidebar />
               <Chat />
             </Route>
-
             <Route path="/">
               <h1>Home Screen</h1>
             </Route>
-          </Routes>
+          </Switch>
         </Router>
       </div>
     </div>
