bda32c7bd2f2f89c7e86b78c0c8b4f4f24448f24
Prerna6060 <112013748+Prerna6060@users.noreply.github.com>
2023-01-08 13:16:12 +0530

    Update SidebarChat.js

--- src/SidebarChat.js
+++ src/SidebarChat.js
@@ -2,32 +2,36 @@ import { Avatar } from "@mui/material";
 import React, { useEffect, useState } from "react";
 import "./SidebarChat.css";
 import db from "./firebase";
+import { Link } from "react-router-dom";
 
 function SidebarChat({ id, name, addNewChat }) {
   const [seed, setSeed] = useState("");
+
   useEffect(() => {
     setSeed(Math.floor(Math.random() * 5000));
   }, []);
 
   const createChat = () => {
-    const roomName = prompt("Please enter name for chat");
+    const roomName = prompt("Please enter name for chat room");
 
     if (roomName) {
       //do something
-      db.collection("Rooms").add({
+      db.collection("rooms").add({
         name: roomName,
       });
     }
   };
 
   return !addNewChat ? (
-    <div className="sidebarChat">
-      <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`} />
-      <div className="sidebarChat_info">
-        <h2>{name}</h2>
-        <p>Last message..</p>
+    <Link to={`/rooms/${id}`}>
+      <div className="sidebarChat">
+        <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`} />
+        <div className="sidebarChat_info">
+          <h2>{name}</h2>
+          <p>Last message..</p>
+        </div>
       </div>
-    </div>
+    </Link>
   ) : (
     <div
       onClick={createChat}
@@ -39,3 +43,4 @@ function SidebarChat({ id, name, addNewChat }) {
 }
 
 export default SidebarChat;
+
